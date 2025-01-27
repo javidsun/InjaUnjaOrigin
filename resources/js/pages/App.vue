019dd3bd3b9b3b3eac1d47000d03c4c24ea88496
zahradev1991 <zahra.azizi709800@gmail.com>
2025-01-28 03:11:20 +0330

    Fix:Admindashboard

--- resources/js/pages/App.vue
+++ resources/js/pages/App.vue
@@ -83,7 +83,7 @@ watch(isDarkMode, (newValue) => {
     --v-surface: #ecf1ff;
     --backgroundcards-color--groups: #2a2a3a;
     --backgroundcards2-color--groups: #cccccc;
-    --backgroundcards3-color--groups: #dbffff;
+    --backgroundcards3-color--groups: #3b3b3b;
     --text-card-color--groups: #cccccc;
     --backgroundcards-color--profile: #2c294f;
     --border-color:#2a2a3b;

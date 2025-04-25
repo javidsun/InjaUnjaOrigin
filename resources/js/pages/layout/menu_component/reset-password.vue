449c6d59aafbabea11fda8d7dcc0dfb112c44837
zahradev1991 <zahra.azizi709800@gmail.com>
2025-04-25 03:32:59 +0330

    fix reset-password Modify

--- resources/js/pages/layout/menu_component/reset-password.vue
+++ resources/js/pages/layout/menu_component/reset-password.vue
@@ -1,116 +1,141 @@
-<script setup>
-//TODO : composition --> option & const & error warning
-
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { t, currentLanguage } from "@/store/languageStore.js";
-import Header from '../Header/Header.vue';
-const setLanguage = (lang) => {
-    t(lang);
-};
-
-const otp = ref('');
-const isOtpInserted = ref(false);
-const router = useRouter();
-
-const form = ref({
-  newPassword: '',
-  confirmPassword: '',
-});
-
-const isPasswordVisible = ref(false);
-const isConfirmPasswordVisible = ref(false);
-
-const handleResetPassword = () => {
-  if (!form.value.newPassword || !form.value.confirmPassword) {
-    alert(messages.value.resetPassword.fillAllFields);
-    return;
-  }
-  if (form.value.newPassword !== form.value.confirmPassword) {
-    alert(messages.value.resetPassword.passwordMismatch);
-    return;
-  }
-  console.log('Resetting password for user:', form.value);
-  router.push({ name: 'login' });
-};
-
-const goToHome = () => {
-  router.push('/');
-};
-</script>
-
 <template>
-    <v-app class="back">
+    <v-app class="background">
         <Header />
-        <br>
-        <br>
-        <br>
+        <br> <br> <br>
 
-    <div class="auth-wrapper d-flex align-center justify-center pa-4">
-      <VCard class="auth-card pa-6 purple-background" style="max-width: 400px;">
-        <VCardText class="text-center">
-          <div class="logo-container form">
-              <img src="/assets/images/inja-unja.png" alt="Logo" class="logo-image" />
-          </div>
-          <h4 class="mb-1 font fontsize">
-              {{ t('resetPassword.greeting') }}
+        <div class="auth-wrapper d-flex align-center justify-center pa-4">
+            <VCard class="auth-card pa-6 purple-background" style="max-width: 400px;">
+                <VCardText class="text-center">
+                    <div class="logo-container form">
+                        <img src="/assets/images/inja-unja.png" alt="Logo" class="logo-image" />
+                    </div>
+                    <h4 class="mb-1 font fontsize">
+                        {{ translate('resetPassword.greeting') }}
+                    </h4>
+                    <p class="font fontsize2">
+                        {{ translate('resetPassword.instructions') }}
+                    </p>
+                </VCardText>
 
-          </h4>
-          <p class="font fontsize2">
-              {{ t('resetPassword.instructions') }}
+                <VForm @submit.prevent="handleResetPassword" class="form_Style">
+                    <VRow>
+                        <VCol cols="12" class="font">
+                            <VTextField
+                                v-model="form.newPassword"
+                                :label="translate('resetPassword.newPassword')"
+                                :placeholder="translate('resetPassword.newPasswordPlaceholder')"
+                                outlined
+                                dense
+                                :type="isPasswordVisible ? 'text' : 'password'"
+                                :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                                @click:append-inner="togglePasswordVisibility"
+                            />
+                        </VCol>
 
-          </p>
-        </VCardText>
+                        <VCol cols="12" class="font">
+                            <VTextField
+                                v-model="form.confirmPassword"
+                                :label="translate('resetPassword.confirmPassword')"
+                                :placeholder="translate('resetPassword.confirmPasswordPlaceholder')"
+                                outlined
+                                dense
+                                :type="isConfirmPasswordVisible ? 'text' : 'password'"
+                                :append-inner-icon="isConfirmPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                                @click:append-inner="toggleConfirmPasswordVisibility"
+                            />
+                        </VCol>
 
-        <VForm @submit.prevent="handleResetPassword" class="form_Style">
-          <VRow>
-            <VCol cols="12" class="font">
-              <VTextField
-                  v-model="form.newPassword"
-                  :label="t('resetPassword.newPassword')"
-                  :placeholder="t('resetPassword.newPasswordPlaceholder')"
-                  outlined
-                  dense
-                  :type="isPasswordVisible ? 'text' : 'password'"
-                  :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-                  @click:append-inner="isPasswordVisible = !isPasswordVisible"
-              />
-            </VCol>
+                        <VCol cols="12" class="form2 mt-0">
+                            <VBtn block type="submit" class="font buttonfont1" color="light-purple">
+                                {{ translate('resetPassword.reset') }}
+                            </VBtn>
+                        </VCol>
+                    </VRow>
+                </VForm>
 
-            <VCol cols="12" class="font">
-              <VTextField
-                  v-model="form.confirmPassword"
-                  :label="t('resetPassword.confirmPassword')"
-                  :placeholder="t('resetPassword.confirmPasswordPlaceholder')"
-                  outlined
-                  dense
-                  :type="isConfirmPasswordVisible ? 'text' : 'password'"
-                  :append-inner-icon="isConfirmPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-                  @click:append-inner="isConfirmPasswordVisible = !isConfirmPasswordVisible"
-              />
-            </VCol>
-
-            <VCol cols="12" class="form2 mt-0">
-              <VBtn block type="submit" class="font buttonfont1" color="light-purple">
-                  {{ t('resetPassword.reset') }}
-              </VBtn>
-            </VCol>
-          </VRow>
-        </VForm>
-
-        <VCardText class="text-center mt-4 form2 fontsize3 font">
-          <p>
-              {{ t('resetPassword.backToLogin') }}
-
-            <RouterLink to="/login" style="color: #4cc8ff" class="fontsize3">
-                {{ t('resetPassword.loginNow') }}
-            </RouterLink>
-          </p>
-        </VCardText>
-      </VCard>
-    </div>
+                <VCardText class="text-center mt-4 form2 fontsize3 font">
+                    <p>
+                        {{ translate('resetPassword.backToLogin') }}
+                        <RouterLink to="/Login" style="color: #4cc8ff" class="fontsize3">
+                            {{ translate('resetPassword.loginNow') }}
+                        </RouterLink>
+                    </p>
+                </VCardText>
+            </VCard>
+        </div>
     </v-app>
 </template>
 
-<style>
+<script>
+import Header from '../Header/Header.vue';
+import { translate } from "@/store/languageStore";
+
+export default {
+    name: 'ResetPassword',
+    components: {
+        Header
+    },
+    data() {
+        return {
+            form: {
+                newPassword: '',
+                confirmPassword: ''
+            },
+            isPasswordVisible: false,
+            isConfirmPasswordVisible: false,
+            messages: {
+                resetPassword: {
+                    fillAllFields: 'Please fill all fields',
+                    passwordMismatch: 'Passwords do not match'
+                }
+            }
+        };
+    },
+    methods: {
+        translate,
+        async handleResetPassword() {
+            try {
+                if (!this.form.newPassword || !this.form.confirmPassword) {
+                    alert(this.messages.resetPassword.fillAllFields);
+                    return;
+                }
+                if (this.form.newPassword !== this.form.confirmPassword) {
+                    alert(this.messages.resetPassword.passwordMismatch);
+                    return;
+                }
+                //TODO: After successful backend response, redirect to login
+
+                await this.$router.push({ name: 'login' });
+            } catch (error) {
+                this.showErrorNotification('Failed to reset password. Please try again.');
+            }
+        },
+
+        showErrorNotification(message) {
+            alert(message);
+        },
+
+        togglePasswordVisibility() {
+            this.isPasswordVisible = !this.isPasswordVisible;
+        },
+
+        toggleConfirmPasswordVisibility() {
+            this.isConfirmPasswordVisible = !this.isConfirmPasswordVisible;
+        },
+
+
+    }
+};
+</script>
+<style scoped>
+.background{
+    background-color: var(--background-color);
+}
 </style>
+
+//TODO:for backend:
+{
+"newPassword",
+"confirmPassword": " must match newPassword",
+"token": " reset token received in email"
+}

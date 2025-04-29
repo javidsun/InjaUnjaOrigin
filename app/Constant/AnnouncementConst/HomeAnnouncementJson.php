fac417c0b5b32b8aa815baf61fca021c9beefbe5
javid <javid.shamsi@xnoova.com>
2025-04-29 13:55:50 +0200

    feats : front end convert to option

--- app/Constant/AnnouncementConst/HomeAnnouncementJson.php
+++ app/Constant/AnnouncementConst/HomeAnnouncementJson.php
@@ -4,13 +4,13 @@ namespace App\Constant\AnnouncementConst;
 
 class HomeAnnouncementJson
 {
-    public const ID='id';
-    public const USER_ID='user_id';
-    public const TITLE='title';
-    public const DESCRIPTION='description';
-    public const PRICE='price';
+    public const ID = 'id';
+    public const USER_ID = 'user_id';
+    public const TITLE = 'title';
+    public const DESCRIPTION = 'description';
+    public const PRICE = 'price';
     public const LOCATION = 'location';
-    public const IMAGE='image';
-    public const CREATED_AT='created_at';
-    public const UPDATED_AT='updated_at';
+    public const IMAGE = 'image';
+    public const CREATED_AT = 'created_at';
+    public const UPDATED_AT = 'updated_at';
 }

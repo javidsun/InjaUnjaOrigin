49f3ba8b36f194eb8151adebf12fd53fd8a45be8
zahradev1991 <zahra.azizi709800@gmail.com>
2025-05-02 21:33:05 +0330

    fix_ads_list

--- resources/js/pages/Ad/AdsList.vue
+++ resources/js/pages/Ad/AdsList.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
-        <h2>آگهی‌های موجود</h2>
+        <h2>Available ads</h2>
         <CategoryFilter @filter="filterAds" />
         <v-list>
-            <v-list-item v-for="ad in filteredAds" :key="ad.id" @click="$router.push(`/ads/${ad.id}`)">
+            <v-list-item
+                v-for="ad in filteredAds"
+                :key="ad.id"
+                @click="navigateToAd(ad.id)"
+            >
                 <v-list-item-content>
                     <v-list-item-title>{{ ad.title }}</v-list-item-title>
                     <v-list-item-subtitle>{{ ad.price }} Euro</v-list-item-subtitle>
@@ -13,29 +17,56 @@
     </div>
 </template>
 
-<script setup>
-// TODO  : composition --> option  &  const & warning & errore
+<script>
+//Todo: id/title/price/category_id/description/created_at/updated_at/images/user_id/location/location
 
-import { ref, onMounted } from "vue";
 import adService from "../../services/adService";
 import CategoryFilter from "../Ad/CategoryFilter.vue";
 
-const ads = ref([]);
-const filteredAds = ref([]);
-
-const fetchAds = async () => {
-    try {
-        const response = await adService.getAds();
-        ads.value = response.data;
-        filteredAds.value = ads.value;
-    } catch (error) {
-        console.error("Error fetching ads:", error);
+export default {
+    components: {
+        CategoryFilter
+    },
+    data() {
+        return {
+            ads: [],
+            filteredAds: []
+        };
+    },
+    methods: {
+        async fetchAds() {
+            try {
+                const response = await adService.getAds();
+                this.ads = response.data;
+                this.filteredAds = this.ads;
+            } catch (error) {
+                this.showError("Error receiving ads", error);
+            }
+        },
+        filterAds(categoryId) {
+            try {
+                this.filteredAds = this.ads.filter(ad => ad.category_id === categoryId);
+            } catch (error) {
+                this.showError("Error filtering ads", error);
+            }
+        },
+        navigateToAd(adId) {
+            try {
+                this.$router.push(`/ads/${adId}`);
+            } catch (error) {
+                this.showError("Error in moving to the ad page", error);
+            }
+        },
+        showError(message, error) {
+            console.error(message, error);
+            this.$alert(message, "error", {
+                type: "error",
+                confirmButtonText: "I understand"
+            });
+        }
+    },
+    mounted() {
+        this.fetchAds();
     }
 };
-
-const filterAds = (categoryId) => {
-    filteredAds.value = ads.value.filter((ad) => ad.category_id === categoryId);
-};
-
-onMounted(fetchAds);
 </script>

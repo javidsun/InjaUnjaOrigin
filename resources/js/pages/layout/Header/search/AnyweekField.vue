279d1afae5982f64273828afab7fe96932fbd34c
zahradev1991 <zahra.azizi709800@gmail.com>
2025-05-04 15:25:40 +0330

    fix_search_anyweek_field

--- resources/js/pages/layout/Header/search/AnyweekField.vue
+++ resources/js/pages/layout/Header/search/AnyweekField.vue
@@ -12,8 +12,8 @@
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="primary" @click="filterByDate">{{ translate('Ad.Search') }}</v-btn>
-                    <v-btn color="grey" text @click="showCalendar = false">{{ translate('Ad.Cancel') }}</v-btn>
+                    <v-btn color="primary" @click="handleDateSelection">{{ translate('Ad.Search') }}</v-btn>
+                    <v-btn color="grey" text @click="closeCalendar">{{ translate('Ad.Cancel') }}</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -21,25 +21,89 @@
 </template>
 
 <script>
-import { translate } from '@/store/languageStore.js';
+import { translate } from '@/store/languageStore';
 
 export default {
-    setup() {
-        return { translate };
-    },
+    name: 'DateRangePicker',
     data() {
         return {
             showCalendar: false,
             selectedDates: [],
             isMobile: false,
+            windowWidth: 0
         };
     },
+    mounted() {
+        try {
+            this.checkMobileView();
+            window.addEventListener('resize', this.handleResize);
+        } catch (error) {
+            this.handleError('Initialization error', error);
+        }
+    },
+    beforeDestroy() {
+        try {
+            window.removeEventListener('resize', this.handleResize);
+        } catch (error) {
+            this.handleError('Cleanup error', error);
+        }
+    },
     methods: {
-        filterByDate() {
-            this.$emit('dates-selected', this.selectedDates);
+        translate,
+
+        handleDateSelection() {
+            try {
+                if (this.selectedDates.length === 0) {
+                    this.showAlert(this.translate('DatePicker.noDatesSelected'));
+                    return;
+                }
+
+                this.$emit('dates-selected', this.selectedDates);
+                this.showCalendar = false;
+                this.showSuccess(this.translate('DatePicker.datesApplied'));
+            } catch (error) {
+                this.handleError('Date selection error', error);
+            }
+        },
+
+        closeCalendar() {
             this.showCalendar = false;
         },
-    },
+
+        checkMobileView() {
+            this.windowWidth = window.innerWidth;
+            this.isMobile = this.windowWidth < 768;
+        },
+
+        handleResize() {
+            try {
+                this.checkMobileView();
+            } catch (error) {
+                this.handleError('Resize handler error', error);
+            }
+        },
+
+        showSuccess(message) {
+            this.$store.dispatch('showNotification', {
+                type: 'success',
+                message: message
+            });
+        },
+
+        showAlert(message) {
+            this.$store.dispatch('showNotification', {
+                type: 'warning',
+                message: message
+            });
+        },
+
+        handleError(context, error) {
+            this.$store.dispatch('showNotification', {
+                type: 'error',
+                message: `${context}: ${error.message}`
+            });
+        }
+    }
 };
 </script>
 

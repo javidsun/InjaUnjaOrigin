017b75e01f2352763d0a0169c62ae3ad5d0b812c
zahradev1991 <zahra.azizi709800@gmail.com>
2025-05-05 20:18:18 +0330

    fix_mainbanner

--- resources/js/pages/layout/sections/MainBanner.vue
+++ resources/js/pages/layout/sections/MainBanner.vue
@@ -1,31 +1,38 @@
-<script setup>
-const props = defineProps({
-  bannerImage: String,
-});
-</script>
-
 <template>
-  <div class="main-banner">
-    <v-img :src="bannerImage" alt="Main Banner" />
-  </div>
+    <div className="main-banner">
+        <v-img :src="bannerImage" alt="Main Banner" />
+    </div>
 </template>
 
+<script>
+export default {
+    name: 'MainBanner',
+    props: {
+        bannerImage: {
+            type: String,
+            required: true,
+            default: ''
+        }
+    },
+}
+</script>
+
 <style>
-@media (max-width: 960px) {
-  .main-banner-container {
-    justify-content: center;
-
-  }
-}
-.main-banner-container {
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
-}
 .main-banner {
-  width: 85%;
-  border-radius: 8px;
-  padding-top: 45px;
+    width: 85%;
+    border-radius: 8px;
+    padding-top: 45px;
+}
 
+@media (max-width: 960px) {
+    .main-banner-container {
+        justify-content: center;
+    }
+}
+
+.main-banner-container {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
 }
 </style>

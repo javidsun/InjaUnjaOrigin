922a6013104abab7276c58913dd3b4890f054889
zahradev1991 <zahra.azizi709800@gmail.com>
2025-05-06 03:13:36 +0330

    fix_reservation_details

--- resources/js/pages/Users/Reserv/ReservationDetails.vue
+++ resources/js/pages/Users/Reserv/ReservationDetails.vue
@@ -1,11 +1,11 @@
-<template >
+<template>
     <UserSidebar>
         <v-container fluid class="back">
             <v-row>
                 <v-col cols="12">
                     <v-card class="mb-4" outlined>
                         <v-card-title class="text-h5 title">
-                            {{ reservation.title }}
+                            {{ currentReservation.title }}
                         </v-card-title>
                     </v-card>
                 </v-col>
@@ -13,21 +13,21 @@
 
             <v-row>
                 <v-col cols="12" md="6">
-                    <v-img :src="reservation.image" class="reservation-image"></v-img>
+                    <v-img :src="currentReservation.image" class="reservation-image"></v-img>
                 </v-col>
                 <v-col cols="12" md="6">
                     <v-card outlined class="details-card">
                         <v-card-title>
-                            <v-icon :color="reservation.color" class="reservation-icon">
-                                {{ reservation.icon }}
+                            <v-icon :color="currentReservation.color" class="reservation-icon">
+                                {{ currentReservation.icon }}
                             </v-icon>
-                            {{ reservation.title }}
+                            {{ currentReservation.title }}
                         </v-card-title>
                         <v-card-subtitle class="reservation-date">
-                            {{ reservation.date }}
+                            {{ currentReservation.date }}
                         </v-card-subtitle>
                         <v-card-text>
-                            <p>{{ reservation.description }}</p>
+                            <p>{{ currentReservation.description }}</p>
                         </v-card-text>
                     </v-card>
                 </v-col>
@@ -36,66 +36,115 @@
     </UserSidebar>
 </template>
 
-<script setup>
-//TODO : composition --> option & const & error warning
-
-import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+<script>
 import UserSidebar from "../Layout.vue";
 
-const props = defineProps({
-    id: String
-});
-
-const reservationId = ref(parseInt(props.id));
-
-const reservations = ref([
-    {
-        id: 1,
-        title: "رزرو خانه",
-        date: "2025-01-01",
-        icon: "mdi-home",
-        color: "blue",
-        image: "/ads/house.svg",
-        description: "رزرو خانه برای تاریخ 2025-01-01",
+export default {
+    name: 'ReservationDetail',
+    components: {
+        UserSidebar
     },
-    {
-        id: 2,
-        title: "رزرو ماشین",
-        date: "2024-12-29",
-        icon: "mdi-car",
-        color: "green",
-        image: "/ads/car.svg",
-        description: "رزرو ماشین برای تاریخ 2024-12-29",
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
     },
-    {
-        id: 3,
-        title: "EventReservation",
-        date: "2024-12-25",
-        icon: "mdi-calendar",
-        color: "purple",
-        image: "/ads/event.svg",
-        description: "رزرو بلیط رویداد برای تاریخ 2024-12-25",
+    data() {
+        return {
+            allReservations: [
+                {
+                    id: 1,
+                    title: "رزرو خانه",
+                    date: "2025-01-01",
+                    icon: "mdi-home",
+                    color: "blue",
+                    image: "/ads/house.svg",
+                    description: "رزرو خانه برای تاریخ 2025-01-01",
+                },
+                {
+                    id: 2,
+                    title: "رزرو ماشین",
+                    date: "2024-12-29",
+                    icon: "mdi-car",
+                    color: "green",
+                    image: "/ads/car.svg",
+                    description: "رزرو ماشین برای تاریخ 2024-12-29",
+                },
+                {
+                    id: 3,
+                    title: "EventReservation",
+                    date: "2024-12-25",
+                    icon: "mdi-calendar",
+                    color: "purple",
+                    image: "/ads/event.svg",
+                    description: "رزرو بلیط رویداد برای تاریخ 2024-12-25",
+                },
+                {
+                    id: 4,
+                    title: "TravelerReservation",
+                    date: "2024-12-20",
+                    icon: "mdi-account-group",
+                    color: "orange",
+                    image: "/ads/firend.svg",
+                    description: "رزرو گروه مسافران برای تاریخ 2024-12-20",
+                },
+            ],
+            currentReservation: {},
+            isLoading: false,
+            error: null
+        };
     },
-    {
-        id: 4,
-        title: "TravelerReservation",
-        date: "2024-12-20",
-        icon: "mdi-account-group",
-        color: "orange",
-        image: "/ads/firend.svg",
-        description: "رزرو گروه مسافران برای تاریخ 2024-12-20",
+    computed: {
+        parsedReservationId() {
+            try {
+                return parseInt(this.id);
+            } catch (error) {
+                this.handleError("Invalid reservation ID format", error);
+                return null;
+            }
+        }
     },
-]);
+    mounted() {
+        this.loadReservation();
+    },
+    methods: {
 
-const reservation = ref({});
+        loadReservation() {
+            try {
+                if (!this.parsedReservationId) {
+                    this.handleError("No valid reservation ID provided");
+                    return;
+                }
 
-onMounted(() => {
-    reservation.value = reservations.value.find((r) => r.id === reservationId.value) || {};
-});
+                this.isLoading = true;
+                const foundReservation = this.allReservations.find(
+                    item => item.id === this.parsedReservationId
+                );
+
+                if (foundReservation) {
+                    this.currentReservation = foundReservation;
+                } else {
+                    this.handleError("Reservation not found");
+                }
+            } catch (error) {
+                this.handleError("Failed to load reservation details", error);
+            } finally {
+                this.isLoading = false;
+            }
+        },
+
+        handleError(message, error) {
+            this.error = message;
+            this.$emit('error', { message, error });
+
+            if (error) {
+                console.error(`Technical Error Details: ${error.message}`);
+            }
+        }
+    }
+};
 </script>
-
-
 
 <style scoped>
 .details-card {
@@ -119,13 +168,13 @@ onMounted(() => {
     font-style: italic;
 }
 
-.title{
+.title {
     background-color: var(--background-color--groups3);
     text-align: center;
     color: var(--text-color);
 }
-.back{
-    background-color: var(--background-color--groups3);
 
+.back {
+    background-color: var(--background-color--groups3);
 }
 </style>

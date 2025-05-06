d9abdd60cab05e659122eae1959bcd093d1b0818
zahradev1991 <zahra.azizi709800@gmail.com>
2025-05-06 16:35:53 +0330

    fix_policy

--- resources/js/pages/Users/SettingModals/Policy.vue
+++ resources/js/pages/Users/SettingModals/Policy.vue
@@ -7,7 +7,6 @@
                 <v-btn icon @click="closePolicyModal" class="close-btn">
                     <v-icon>mdi-close</v-icon>
                 </v-btn>
-
             </v-card-title>
             <v-divider></v-divider>
 
@@ -21,21 +20,17 @@
                 <div class="policy-section">
                     <h3 class="section-title">{{ translate('policy.supplementaryDocumentsTitle') }}</h3>
                     <ul class="document-list">
-                        <li>{{ translate('policy.outsideUS') }}</li>
-                        <li>{{ translate('policy.stateSupplements') }}</li>
-                        <li>{{ translate('policy.cookiePolicy') }}</li>
-                        <li>{{ translate('policy.enterpriseCustomers') }}</li>
-                        <li>{{ translate('policy.chinaPrivacySupplement') }}</li>
-                        <li>{{ translate('policy.colombiaOnly') }}</li>
-                        <li>{{ translate('policy.turkeyOnly') }}</li>
-                        <li>{{ translate('policy.dac7Notice') }}</li>
+                        <li v-for="(document, index) in supplementaryDocuments" :key="index">
+                            {{ translate(document) }}
+                            <v-icon v-if="hasAlert(document)" color="warning" small class="ml-1">mdi-alert</v-icon>
+                        </li>
                     </ul>
                 </div>
                 <v-divider></v-divider>
 
                 <div class="policy-section">
-                    <h3 class="section-title">{{translate ('policy.relatedArticlesTitle') }}</h3>
-                    <p class="section-description">{{ translate ('policy.relatedArticlesDescription') }}</p>
+                    <h3 class="section-title">{{ translate('policy.relatedArticlesTitle') }}</h3>
+                    <p class="section-description">{{ translate('policy.relatedArticlesDescription') }}</p>
                 </div>
                 <v-divider></v-divider>
 
@@ -50,27 +45,74 @@
                     <p class="section-description">{{ translate('policy.termsUpdatesDescription') }}</p>
                 </div>
             </v-card-text>
+
+            <v-card-actions>
+                <v-btn color="error" @click="showConfirmation" class="mr-4">
+                    <v-icon left>mdi-alert-circle</v-icon>
+                    {{ translate('policy.importantNotice') }}
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="acceptPolicy">
+                    {{ translate('policy.acceptButton') }}
+                </v-btn>
+            </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
 
-<script setup>
-//TODO : composition --> option & const & error warning
+<script>
+import { translate } from "@/store/languageStore";
 
-import { ref } from 'vue';
-import { translate } from "@/store/languageStore.js";
-
-const ispolicyModalOpen = ref(false);
-
-const openModal = () => {
-    ispolicyModalOpen.value = true;
-};
-
-const closePolicyModal = () => {
-    ispolicyModalOpen.value = false;
-};
-
-defineExpose({ openModal });
+export default {
+    name: 'PolicyModal',
+    data() {
+        return {
+            ispolicyModalOpen: false,
+            supplementaryDocuments: [
+                'policy.outsideUS',
+                'policy.stateSupplements',
+                'policy.cookiePolicy',
+                'policy.enterpriseCustomers',
+                'policy.chinaPrivacySupplement',
+                'policy.colombiaOnly',
+                'policy.turkeyOnly',
+                'policy.dac7Notice'
+            ],
+            documentsWithAlerts: [
+                'policy.chinaPrivacySupplement',
+                'policy.dac7Notice'
+            ]
+        }
+    },
+    methods: {
+        translate,
+        openModal() {
+            this.ispolicyModalOpen = true;
+        },
+        closePolicyModal() {
+            this.ispolicyModalOpen = false;
+        },
+        hasAlert(documentKey) {
+            return this.documentsWithAlerts.includes(documentKey);
+        },
+        showConfirmation() {
+            this.$emit('show-alert', {
+                title: this.translate('policy.importantNoticeTitle'),
+                message: this.translate('policy.importantNoticeMessage'),
+                type: 'warning'
+            });
+        },
+        acceptPolicy() {
+            this.$emit('policy-accepted');
+            this.closePolicyModal();
+            this.$emit('show-alert', {
+                title: this.translate('policy.thankYouTitle'),
+                message: this.translate('policy.thankYouMessage'),
+                type: 'success'
+            });
+        }
+    }
+}
 </script>
 
 <style scoped>
@@ -109,12 +151,14 @@ defineExpose({ openModal });
     font-size: 1.1rem;
     font-weight: bold;
     margin-bottom: 10px;
+    text-transform: uppercase;
 }
 
 .section-description {
-    color: white;
+    color: var(--text-color);
     font-size: 0.9rem;
     margin-bottom: 10px;
+    line-height: 1.6;
 }
 
 .document-list {
@@ -126,6 +170,15 @@ defineExpose({ openModal });
 .document-list li {
     color: white;
     font-size: 0.9rem;
-    margin-bottom: 5px;
+    margin-bottom: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.v-card-actions {
+    padding: 16px;
+    background-color: var(--background-color);
+    border-top: 1px solid #ddd;
 }
 </style>
